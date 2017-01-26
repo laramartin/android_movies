83267e82679485bd67e2a36536d4fcb19901d33b
Lara Martin <lara.martin.carretero@gmail.com>
2017-01-26 17:37:22 +0900

    start details activity

--- app/src/main/java/eu/laramartin/popularmovies/MoviesAdapter.java
+++ app/src/main/java/eu/laramartin/popularmovies/MoviesAdapter.java
@@ -1,5 +1,6 @@
 package eu.laramartin.popularmovies;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,13 +31,22 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesAdap
         notifyDataSetChanged();
     }
 
-    public class MoviesAdapterViewHolder extends RecyclerView.ViewHolder {
+    public class MoviesAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         final ImageView imagePoster;
 
         public MoviesAdapterViewHolder(View itemView) {
             super(itemView);
             imagePoster = (ImageView) itemView.findViewById(R.id.image_poster);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(view.getContext(), DetailsActivity.class);
+            Movie currentMovie = movies.get(getAdapterPosition());
+            intent.putExtra("movieDetails", currentMovie);
+            view.getContext().startActivity(intent);
         }
     }
 
--- app/src/main/java/eu/laramartin/popularmovies/data/Movie.java
+++ app/src/main/java/eu/laramartin/popularmovies/data/Movie.java
@@ -1,9 +1,12 @@
 package eu.laramartin.popularmovies.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by lara on 24/1/17.
  */
-public class Movie {
+public class Movie implements Parcelable {
 
     private String posterPath;
     private String overview;
@@ -11,6 +14,30 @@ public class Movie {
     private String title;
     private int id;
     private long voteAverage;
+
+    public Movie() {
+    }
+
+    public Movie(Parcel in) {
+        posterPath = in.readString();
+        overview = in.readString();
+        releaseDate = in.readString();
+        title = in.readString();
+        id = in.readInt();
+        voteAverage = in.readLong();
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 
     public String getPosterPath() {
         return posterPath;
@@ -58,5 +85,20 @@ public class Movie {
 
     public void setVoteAverage(long voteAverage) {
         this.voteAverage = voteAverage;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(posterPath);
+        dest.writeString(overview);
+        dest.writeString(releaseDate);
+        dest.writeString(title);
+        dest.writeInt(id);
+        dest.writeLong(voteAverage);
     }
 }
