b68fbcd8a1bfbef37d8658bcfa32e95aa6cd238d
Lara Martin <lara.martin.carretero@gmail.com>
2017-01-26 17:14:26 +0900

    create details activity

---
+++ app/src/main/java/eu/laramartin/popularmovies/DetailsActivity.java
@@ -0,0 +1,15 @@
+package eu.laramartin.popularmovies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import eu.laramartin.popularmovies.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
