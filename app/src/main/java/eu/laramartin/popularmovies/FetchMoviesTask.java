75975478b4ea972321efca3630d8a5d007624459
Lara Martin <larikimc@gmail.com>
2017-02-11 12:29:13 +0100

    extract AsynTask in a class

---
+++ app/src/main/java/eu/laramartin/popularmovies/FetchMoviesTask.java
@@ -0,0 +1,45 @@
+package eu.laramartin.popularmovies;
+
+import android.os.AsyncTask;
+
+import java.net.URL;
+import java.util.List;
+
+import eu.laramartin.popularmovies.api.MoviesJsonUtils;
+import eu.laramartin.popularmovies.api.NetworkUtils;
+import eu.laramartin.popularmovies.data.Movie;
+import eu.laramartin.popularmovies.data.MoviesResponse;
+
+/**
+ * Created by lara on 11.02.17.
+ */
+public class FetchMoviesTask extends AsyncTask<String, Void, List<Movie>> {
+
+    private MoviesAdapter adapter;
+
+    public FetchMoviesTask(MoviesAdapter adapter) {
+        this.adapter = adapter;
+    }
+
+    @Override
+    protected List<Movie> doInBackground(String... params) {
+        URL moviesRequestUrl = NetworkUtils.buildUrl(BuildConfig.API_KEY, params[0]);
+        try {
+            String jsonMoviesResponse = NetworkUtils
+                    .getResponseFromHttpUrl(moviesRequestUrl);
+            MoviesResponse moviesResponse = MoviesJsonUtils.parseJson(jsonMoviesResponse);
+            return moviesResponse.getMovies();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(List<Movie> movies) {
+        if (movies != null) {
+//            Log.v(MainActivity.LOG_TAG, "moviesData: " + movies);
+            adapter.setMoviesData(movies);
+        }
+    }
+}
--- app/src/main/java/eu/laramartin/popularmovies/MainActivity.java
+++ app/src/main/java/eu/laramartin/popularmovies/MainActivity.java
@@ -1,24 +1,15 @@
 package eu.laramartin.popularmovies;
 
 import android.content.res.Configuration;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.net.URL;
-import java.util.List;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import eu.laramartin.popularmovies.api.MoviesJsonUtils;
-import eu.laramartin.popularmovies.api.NetworkUtils;
-import eu.laramartin.popularmovies.data.Movie;
-import eu.laramartin.popularmovies.data.MoviesResponse;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,12 +26,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        FetchMoviesTask moviesTask = new FetchMoviesTask();
-        moviesTask.execute(FILTER_TYPE_1);
         GridLayoutManager layoutManager = new GridLayoutManager(this, getSpan());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
         adapter = new MoviesAdapter();
+        FetchMoviesTask moviesTask = new FetchMoviesTask(adapter);
+        moviesTask.execute(FILTER_TYPE_1);
         recyclerView.setAdapter(adapter);
     }
 
@@ -49,31 +40,6 @@ public class MainActivity extends AppCompatActivity {
             return 4;
         }
         return 2;
-    }
-
-    public class FetchMoviesTask extends AsyncTask<String, Void, List<Movie>> {
-
-        @Override
-        protected List<Movie> doInBackground(String... params) {
-            URL moviesRequestUrl = NetworkUtils.buildUrl(BuildConfig.API_KEY, params[0]);
-            try {
-                String jsonMoviesResponse = NetworkUtils
-                        .getResponseFromHttpUrl(moviesRequestUrl);
-                MoviesResponse moviesResponse = MoviesJsonUtils.parseJson(jsonMoviesResponse);
-                return moviesResponse.getMovies();
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(List<Movie> movies) {
-            if (movies != null) {
-                Log.v(LOG_TAG, "moviesData: " + movies);
-                adapter.setMoviesData(movies);
-            }
-        }
     }
 
     @Override
@@ -85,11 +51,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.action_popular) {
-            FetchMoviesTask moviesTask = new FetchMoviesTask();
+            FetchMoviesTask moviesTask = new FetchMoviesTask(adapter);
             moviesTask.execute(FILTER_TYPE_1);
         }
         if (item.getItemId() == R.id.action_top_rated) {
-            FetchMoviesTask moviesTask = new FetchMoviesTask();
+            FetchMoviesTask moviesTask = new FetchMoviesTask(adapter);
             moviesTask.execute(FILTER_TYPE_2);
         }
         return super.onOptionsItemSelected(item);
